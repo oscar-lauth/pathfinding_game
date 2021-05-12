f13dbfdc05e8679588a81ad2309e4a625db049bf
Oscar Lauth <oscar.lauth@gmail.com>
2021-05-12 00:09:29 -0400

    Test for robot sprite with CLinkedList

--- MapPanel.java
+++ MapPanel.java
@@ -4,6 +4,8 @@ import java.awt.*;
 public class MapPanel extends JPanel{
 	private static Tile[][] gameMap;
 	private int ROW_DIMENSION, COL_DIMENSION;
+	private static CLinkedList<ImageIcon> robotSprite;
+	private static int spriteIndex = 0;
 	//private static LinkedList
 
 	public MapPanel() { //for creating default blank map
@@ -21,9 +23,19 @@ public class MapPanel extends JPanel{
 			this.add(gameMap[r][c]);
 			}
 		}
+		initSprite();
+		gameMap[28][28].setIcon(robotSprite.get(spriteIndex));
+		spriteIndex++;
 		// JButton btn = new JButton("PRESS TO SAVE MAP:");
 		// btn.addActionListener(new ButtonListener());
 		// this.add(btn);
+	}
+	public void initSprite() {
+		robotSprite = new CLinkedList<ImageIcon>();
+		for(int i=0;i<=7;i++) {
+			String spritePath = "robot_sprite/robot_sprite_"+i+".png";
+			robotSprite.add(new ImageIcon(spritePath));
+		}
 	}
 	// public MapPanel(Tile[][] gMap) {
 	// 	gameMap = gMap;
@@ -66,18 +78,26 @@ public class MapPanel extends JPanel{
 			Tile t = (Tile)e.getSource();
 			if(e.getButton()==MouseEvent.BUTTON1) { //mouse left click
 				t.createWall();
+				gameMap[28][28].setIcon(robotSprite.get(spriteIndex));
+				spriteIndex++;
 			}
 			else if(e.getButton()==MouseEvent.BUTTON3) { //mouse right click
 				t.removeWall();
+				gameMap[28][28].setIcon(robotSprite.get(spriteIndex));
+				spriteIndex++;
 			}
 		}
 		public void mousePressed(MouseEvent e) { //mousePressed allows precise map creation
 			Tile t = (Tile)e.getSource();
 			if(e.getButton()==MouseEvent.BUTTON1) { //mouse left click
 				t.createWall();
+				gameMap[28][28].setIcon(robotSprite.get(spriteIndex));
+				spriteIndex++;
 			}
 			else if(e.getButton()==MouseEvent.BUTTON3) { //mouse right click
 				t.removeWall();
+				gameMap[28][28].setIcon(robotSprite.get(spriteIndex));
+				spriteIndex++;
 			}
 		}
 	}
